9b9b5bef112a67a4ef4d7909afa54efb60a4c0fd
Ryan <rburgess@evernote.com>
2014-04-06 22:57:26 -0700

    add sass to the build and clean up files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,48 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-  juice: {
-    options: {
-      removeStyleTags: false
+    sass: {
+      dist: {
+        files: [
+          {
+            expand: true,
+            cwd: 'sass/',
+            src: ['**/*.scss'],
+            dest: 'emails/css/',
+            ext: '.css',
+          },
+        ],
+      },
     },
-    dynamic_mappings: {
-       files: [
-         {
-           expand: true,
-           cwd: 'emails/',
-           src: ['**/*.html'],
-           dest: 'build/',
-           ext: '-final.html'
-         }
-       ]
-    }
-   },
-   watch: {
-     build: {
-        files: ['emails/**/*'],
-        tasks: ['juice']
-   },
-   livereload: {
-      files: ['*.html', '*.php', 'js/**/*.{js,json}', 'css/*.css','img/**/*.{png,jpg,jpeg,gif,webp,svg}'],
+    juice: {
       options: {
-        livereload: true
+        removeStyleTags: false
+      },
+      dynamic_mappings: {
+        files: [
+          {
+             expand: true,
+             cwd: 'emails/',
+             src: ['**/*.html'],
+             dest: 'build/',
+             ext: '-final.html'
+          }
+        ]
+      }
+    },
+    watch: {
+      build: {
+          files: ['sass/**/*'],
+          tasks: ['sass', 'juice']
+      },
+      livereload: {
+        files: ['*.html', '*.php', 'js/**/*.{js,json}', 'css/*.css','img/**/*.{png,jpg,jpeg,gif,webp,svg}'],
+        options: {
+          livereload: true
+        }
       }
     }
-   }
- });
+  });
+  grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-juice-email');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.registerTask('default',['watch']);
